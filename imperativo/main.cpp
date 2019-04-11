deeb34bc05c6acf0455790cdf0a3e57de47c49c0
paulojuniore <paulo.junior@ccc.ufcg.edu.br>
2019-04-11 20:56:13 -0300

    testing

--- imperativo/main.cpp
+++ imperativo/main.cpp
@@ -4,9 +4,24 @@
 #include "include/stringService.h"
 #include "include/fileService.h"
 
+#include "jsoncpp/json/json.h"
+
 using namespace std;
 
 int main(){
-    
-    return 0;
+    Json::Value root;
+
+    root["id"]=0;
+            Json::Value text;
+            text["first"]="c++";
+            text["second"]="java";
+            text["third"]="js";
+    root["text"]=text;
+    root["type"]="test";
+    root["begin"]=1;
+    root["end"]=1;
+    Json::StyledWriter writer;
+    string strJson=writer.write(root);
+
+    cout<<"JSON WriteTest" << endl << strJson <<endl;
 }
