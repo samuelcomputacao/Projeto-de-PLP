45771247c18455114e76228f96f551f96f896d45
samuelcomputacao <samuel25101998@gmail.com>
2019-05-02 21:29:52 -0300

    Ajuste na validação do menu inicial

--- funcional/main.hs
+++ funcional/main.hs
@@ -28,6 +28,7 @@ module Main where
         if op == "1" || op == "2" || op == "3" then
             return op
             else do
+                putStrLn "Opcao incorreta!\n Por favor tecle 1,2 ou 3: "
                 op <- getOpcao    
                 return op
 
