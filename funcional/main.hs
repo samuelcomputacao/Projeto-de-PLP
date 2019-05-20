dd671a64e3579df1d5be9a6096fa92caff721931
Rick <richelton14@gmail.com>
2019-05-20 08:37:40 -0400

    Corrigindo rodada 3

--- funcional/main.hs
+++ funcional/main.hs
@@ -53,16 +53,7 @@ module Main where
 
     iniciaJogo::Int -> IO()
     iniciaJogo salario = do
-        rodada1 salario 3 []     
-
-    geraPerguntaRodada1 :: Int -> Either String [Pergunta] -> IO()
-    geraPerguntaRodada1 id e = putStrLn (carregarPerguntaRodada1 id e)
-
-    geraPerguntaRodada2 :: Int -> Either String [PerguntaRodada2] -> IO()
-    geraPerguntaRodada2 id e = putStrLn (carregarPerguntaRodada2 id e)
-
-    geraPerguntaRodada3 :: Int -> Either String [PerguntaRodada3] -> IO()
-    geraPerguntaRodada3 id e = putStrLn (carregarPerguntaRodada3 id e)
+        rodada1 salario 3 []
 
     rodada1::Int -> Int -> [Int] -> IO()
     rodada1 salario quant perguntas = do
@@ -91,46 +82,6 @@ module Main where
             else do
                 putStrLn (errouToString valor 1 (atualizaSalario salario valor False) 1)
                 rodada1 (atualizaSalario salario valor False) (quant - 1) (perguntas ++ [id])
-           
-
-    gerarId:: [Int] -> Int -> IO Int
-    gerarId perguntas maximo = do
-        id <- gerarRandon maximo
-        if elem id perguntas then do
-            id <- gerarId perguntas maximo
-            return id
-        else return id
-    
-    -- Carrega um valor do arquivo para ser usado com o multiplicador
-    gerarValorRodada1 :: Either String [Pontos] -> IO Int
-    gerarValorRodada1 g = do
-        id <- gerarRandon 4
-        return (buscaValorPremioRodada1 id g)
-
-    gerarValorRodada2 :: Either String [PontosRodada2] -> IO Int
-    gerarValorRodada2 g = do
-        id <- gerarRandon 4
-        return (buscaValorPremioRodada2 id g)
-
-    gerarValorRodada3 :: Either String [PontosRodada3] -> IO Int
-    gerarValorRodada3 g = do
-        id <- gerarRandon 4
-        return (buscaValorPremioRodada3 id g)
-
-    getResposta::Int -> IO String
-    getResposta rodada = do
-        op <- getLine
-        if contem op (if rodada == 1 then "ab" else "abcd") then return op
-        else do
-            putStrLn ("Alternativa incorreta!\nPor favor tecle: " ++ (if rodada == 1 then "(a ou b)." else "(a, b, c ou d)."))
-            op <- getResposta rodada
-            return op
-                
-    contem:: [Char] -> [Char] -> Bool
-    contem elemento []  = False
-    contem (x:xs) (h:t) = do
-        if x == h then True
-        else contem (x:xs) t
 
     rodada2::Int -> Int -> [Int] -> IO()
     rodada2 salario quant perguntas = do
@@ -198,18 +149,82 @@ module Main where
             
             if (verificarRespostaRodada3 id resposta f) then do
                  
-                 putStrLn (acertouToString 100 premioPergunta (atualizaSalario salario premioPergunta True)3)
+                 putStrLn (acertouToString valor premioPergunta (atualizaSalario salario premioPergunta True)3)
                  rodada3 (atualizaSalario salario premioPergunta True) (quant - 1) (perguntas ++ [id])
             else do
-                 putStrLn (errouToString 100 premioPergunta (atualizaSalario salario premioPergunta False) 3)
+                 putStrLn (errouToString valor premioPergunta (atualizaSalario salario premioPergunta False) 3)
                  rodada3 (atualizaSalario salario premioPergunta False) (quant - 1) (perguntas ++ [id])
 
+    
+    geraPerguntaRodada1 :: Int -> Either String [Pergunta] -> IO()
+    geraPerguntaRodada1 id e = putStrLn (carregarPerguntaRodada1 id e)
+
+    geraPerguntaRodada2 :: Int -> Either String [PerguntaRodada2] -> IO()
+    geraPerguntaRodada2 id e = putStrLn (carregarPerguntaRodada2 id e)
+
+    geraPerguntaRodada3 :: Int -> Either String [PerguntaRodada3] -> IO()
+    geraPerguntaRodada3 id e = putStrLn (carregarPerguntaRodada3 id e)
+
+    gerarId:: [Int] -> Int -> IO Int
+    gerarId perguntas maximo = do
+        id <- gerarRandon maximo
+        if elem id perguntas then do
+            id <- gerarId perguntas maximo
+            return id
+        else return id
+    
+    -- Carrega um valor do arquivo para ser usado com o multiplicador
+    gerarValorRodada1 :: Either String [Pontos] -> IO Int
+    gerarValorRodada1 g = do
+        id <- gerarRandon 4
+        return (buscaValorPremioRodada1 id g)
+
+    gerarValorRodada2 :: Either String [PontosRodada2] -> IO Int
+    gerarValorRodada2 g = do
+        id <- gerarRandon 4
+        return (buscaValorPremioRodada2 id g)
+
+    gerarValorRodada3 :: Either String [PontosRodada3] -> IO Int
+    gerarValorRodada3 g = do
+        id <- gerarRandon 4
+        return (buscaValorPremioRodada3 id g)
+
+    getResposta::Int -> IO String
+    getResposta rodada = do
+        op <- getLine
+        if contem op (if rodada == 1 then "ab" else "abcd") then return op
+        else do
+            putStrLn ("Alternativa incorreta!\nPor favor tecle: " ++ (if rodada == 1 then "(a ou b)." else "(a, b, c ou d)."))
+            op <- getResposta rodada
+            return op
+
+    getMultiplicador::Int -> IO String
+    getMultiplicador rodada = do
+        mult <- getLine
+        if ((rodada == 2 && (elem mult ["1x","2x"])) || (rodada == 3 && (elem mult ["1x","2x","3x"]))) then return mult
+        else do
+            putStrLn ((if rodada == 2 then "Digite um valor de acordo com as opções dadas(1x e 2x): " else "Digite um valor de acordo com as opções dadas(1x, 2x ou 3x): "))
+            mult <- getMultiplicador rodada
+            return mult 
+
+    parseMultiplicador :: String -> Int
+    parseMultiplicador multi = digitToInt (head multi) :: Int 
+
+    atualizaSalario :: Int -> Int -> Bool -> Int
+    atualizaSalario salario gap status
+        | status == True = salario + gap
+        | otherwise = (if(salario - gap) < 0 then 0 else salario - gap)
+                
+    contem:: [Char] -> [Char] -> Bool
+    contem elemento []  = False
+    contem (x:xs) (h:t) = do
+        if x == h then True
+        else contem (x:xs) t
     
     fimJogo:: Int -> IO()
     fimJogo salario = do
         putStrLn ("Fim de Jogo!\nSeu Saldo final é de: " ++ (show salario) ++"\n\n")
         sairOuJogarNovamente
-    
 
     sairOuJogarNovamente :: IO()
     sairOuJogarNovamente = do
@@ -220,7 +235,6 @@ module Main where
         else do
             putStrLn "\nAté a próxima!\n"
             putStrLn "The System finalized...\n"
-
     
     verificaSaida :: IO String 
     verificaSaida = do
@@ -241,21 +255,3 @@ module Main where
         threadDelay 4000000
         putStr("Tudo pronto! Hora de jogar!\n\n")
         putStrLn logoJogo
-        
-
-    getMultiplicador::Int -> IO String
-    getMultiplicador rodada = do
-        mult <- getLine
-        if ((rodada == 2 && (elem mult ["1x","2x"])) || (rodada == 3 && (elem mult ["1x","2x","3x"]))) then return mult
-        else do
-            putStrLn ((if rodada == 2 then "Digite um valor de acordo com as opções dadas(1x e 2x): " else "Digite um valor de acordo com as opções dadas(1x, 2x ou 3x): "))
-            mult <- getMultiplicador rodada
-            return mult 
-
-    parseMultiplicador :: String -> Int
-    parseMultiplicador multi = digitToInt (head multi) :: Int 
-
-    atualizaSalario :: Int -> Int -> Bool -> Int
-    atualizaSalario salario gap status
-        | status == True = salario + gap
-        | otherwise = (if(salario - gap) < 0 then 0 else salario - gap)
