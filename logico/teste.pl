ab5086a6207d2f68c0d53455573a21c2943855bc
Rickecr <richelton14@gmail.com>
2019-06-09 18:24:47 -0300

    Leitura de arquivos json: Concluído

--- logico/teste.pl
+++ logico/teste.pl
@@ -3,6 +3,11 @@
 
 
 principal:- 
-    lerAlternativas(T, 1), percorrer(T).
+    lervalor(T, 1),
+    percorrer(T),
+    lervalor(A, 2),
+    percorrer(A),
+    lervalor(B, 3),
+    percorrer(B).
 
-percorrer([X|L]) :- write(X.alternativas).
+percorrer([X|L]) :- writeln(X.valor).
